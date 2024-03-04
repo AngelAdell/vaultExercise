83d89b72798fc0a3d8b7e4fa05d6c8a64b616a17
Angel Adell <angeldeniseadell@gmail.com>
2024-03-04 17:15:27 -0500

    solutions to the assignment

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Steps to completing the challenge:
 
 1. create directory and files
+2. Create Html form with instructions and text box input
+3. Add DOM to javascript file that allows the user to understand the problem and be presented with a series of alerts with arithmetic problems.
+4. Add a document listener to the submit button with the event of click that checks the users answers and subsequently presents an alert of success or failure.
+5. Debug
+6. Try Again
--- combo.js
+++ combo.js
@@ -1,1 +1,28 @@
-alert
+
+document.addEventListener("DOMContentLoaded", function() {
+// Alert Questions for the user to determine the secret combination
+alert("You will be asked a series of questions to determine the secret combination. Please answer the following questions to the best of your ability.");
+
+// Alert Questions for the user to determine the secret combination
+alert( "First Combo: What is 5+5? " + "Input your answer for Code 1");
+alert("Second Combo: What is 80 / 2? " + "Input your answer for Code 2");
+alert("Third Combo: What is 78 / 2? " + "Input your answer for Code 3");
+
+const code1 = 10;
+const code2 = 40;
+const code3 = 39;
+
+const form = document.getElementById("codeForm");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const input1 = document.getElementById("code").value;
+    const input2 = document.getElementById("code2").value;
+    const input3 = document.getElementById("code3").value;
+
+    if (input1 == code1 && input2 == code2 && input3 == code3) {
+        alert("Congratulations! You have successfully opened the vault.");
+    } else {
+        alert("Sorry, that is not the correct combination. Please try again.");
+    }
+  });
+});
